ec76d7067b228726570a5e4b5cafe392046a651e
Kowtha Saketh <kowtha.saketh@outlook.com>
2019-12-18 19:22:54 +0530

    Ex7, 8, 9 Done Pending: 2.5.c, 7.2,8.3

---
+++ week-1/Bash/RepetitionWhileThree.sh
@@ -0,0 +1,16 @@
+heads=0
+tails=0
+
+while [ $heads -ne 11 ] && [ $tails -ne 11 ]
+do
+    rand=$((RANDOM % 2))
+    if [ $rand -eq 0 ]
+    then
+        ((heads++))
+    else
+        ((tails++))
+    fi
+    echo "Heads -> $heads and Tail -> $tails"
+done
+
+echo $((heads > tails ? "Heads Won" : "Tail Won"))
---
+++ week-1/Bash/RepetitionWhileTwo.sh
@@ -0,0 +1,9 @@
+echo Choose number between 1 - 100
+
+max=100
+opt
+while [  ]
+do
+
+done
+
