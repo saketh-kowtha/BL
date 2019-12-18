67258ec731f4385c5753e12bcdfff50ad4da568b
Kowtha Saketh <kowtha.saketh@outlook.com>
2019-12-18 11:52:37 +0530

    Ex 6 Done

---
+++ week-1/Bash/RepetitionForFive.sh
@@ -0,0 +1,9 @@
+read -p "Enter Number : " n
+
+fact=1
+for ((i=2;i<=n;i++))
+do
+    fact=$((fact * i))
+done
+
+echo "$fact is Factorial for $n" 
---
+++ week-1/Bash/RepetitionForOne.sh
@@ -0,0 +1,6 @@
+read -p "Enter N : " n
+
+for (( i=1; i<=$n; i++ ))
+do
+    echo "2 ^ $i = $((n ** i))"
+done
