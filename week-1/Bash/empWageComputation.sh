14f1bff2b7069a0189fe0a80bc941819459a453e
Kowtha Saketh <kowtha.saketh@outlook.com>
2019-12-19 17:02:34 +0530

    stored daily and total wages

--- week-1/Bash/empWageComputation.sh
+++ week-1/Bash/empWageComputation.sh
@@ -24,18 +24,24 @@ empWorkingDays=0
 workingDayPerMonth=20
 totalWage=0
 
+declare -A dailyWages
+
 days=30
 while [ days -ne 0 ]
 do
     case $((getAttendece)) in 
-        0)  ;;#Absent
-        1) ((empWorkingDays++));;#Present
+        0)  
+            ;;#Absent
+        1)  ((empWorkingDays++))
+            totalWage=$((totalWage + dailyWage))
+            $dailyWages[$empWorkingDays]=$dailyWage ;;#Present
     esac
     if [ $empWorkingDays -ge 20 ] || [ $((empWorkingDays*workingHours)) -ge 100 ]
     then
         break
     fi
     ((days--))
+
 done
 
 
