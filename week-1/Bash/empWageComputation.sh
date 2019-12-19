f29d43893196e6aa7e5d6553a6305083d5b71c8f
Kowtha Saketh <kowtha.saketh@outlook.com>
2019-12-19 16:57:44 +0530

    Calculated wages based on condition

--- week-1/Bash/empWageComputation.sh
+++ week-1/Bash/empWageComputation.sh
@@ -1,15 +1,42 @@
 echo Welcome to Employee Wage Computation
 
-employeeAttendence=$((RANDOM%2))
+function getAttendece(){
+    echo $((RANDOM%2))
+}
 
-wagePerHour=20
-workingHours=8
+function dailyWage(){
+    local wagePerHour=20
+    local workingHours=8
+    echo $((wagePerHour * workingHours))
+}
+
+function getTotalWorkHours(){
+    local workingHours=8
+    echo $((empWorkingDays * workingHours))
+}
 
 partTimeHour=8
 
-case $employeeAttendence in 
-    0) ;;#Absent
-    1) ;;#Present
-esac
+empWorkingDays=0
+
+
 
 workingDayPerMonth=20
+totalWage=0
+
+days=30
+while [ days -ne 0 ]
+do
+    case $((getAttendece)) in 
+        0)  ;;#Absent
+        1) ((empWorkingDays++));;#Present
+    esac
+    if [ $empWorkingDays -ge 20 ] || [ $((empWorkingDays*workingHours)) -ge 100 ]
+    then
+        break
+    fi
+    ((days--))
+done
+
+
+
