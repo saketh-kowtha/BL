a680640aea96834fb332d65eb63c6e845b76e7a4
Kowtha Saketh <kowtha.saketh@outlook.com>
2019-12-19 13:14:34 +0530

    7.2 Done 2.5.c -> Need to discuss

--- week-1/Bash/RepetitionWhileTwo.sh
+++ week-1/Bash/RepetitionWhileTwo.sh
@@ -1,9 +1,20 @@
-echo Choose number between 1 - 100
 
-max=100
-opt
-while [  ]
+echo Choose a number between 1 - 100
+
+right=100
+left=0
+
+while [ $left -lt $right ] && [ $((right - left)) -ne 2 ]
 do
-
+echo $left $right
+    
+    read -p "Is It greater than $(( (right + left) /2)) ? [y/n] : " ans
+    if [ $ans == "y" ]
+    then
+        left=$(( (left + right) /2))
+    else
+        right=$(( (left + right) /2))
+    fi
 done
 
+echo $((left + 1)) is your number
